f4c016d6021e54b44098e3a6a4b0d087209e7dd7
Kamehamehaaaaaa <sushantsamani@gmail.com>
2023-06-01 14:43:25 +0530

    Completed Scene

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera followCamera;
+    [SerializeField] GameObject startUI;
+    [SerializeField] CarController carController;
+
+    public void SwitchCamera()
+    {
+        followCamera.gameObject.SetActive(true);
+        startUI.SetActive(false);
+        carController.enabled = true;
+    }
+}
